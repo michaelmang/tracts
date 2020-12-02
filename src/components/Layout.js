59f34e6108bec41381c84a4ff34a441afe56583e
Mike Mangialardi <mike.mangialardi@rackspace.com>
2020-12-02 16:47:18 -0500

    Add footer cred

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,12 @@ export default function Layout({ children }) {
   return (
     <div className="min-h-screen" style={{ backgroundColor: "#181818" }}>
       {children}
-      <div className="h-16 w-full"></div>
+      <div className="h-4 sm:h-16 w-full text-white flex items-center justify-center sm:justify-start text-xs sm:pl-4 py-4 sm:p-12 py-10">
+        Created by Michael Mangialardi <span className="mx-2">|</span>
+        <a href="https://www.buymeacoffee.com/fgn7ts2" target="_blank">
+          <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" className="h-8 w-auto" />
+        </a>
+      </div>
     </div>
   );
 }
