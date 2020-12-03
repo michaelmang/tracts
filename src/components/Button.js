dbcdf18510b42c9c6b6140257b85d8324d82e1eb
Mike Mangialardi <mike.mangialardi@rackspace.com>
2020-12-03 16:13:11 -0500

    Implement share/copy button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,10 @@
-export default function Button({ children, isSecondary = false, size = 'sm' }) {
+export default function Button({ children, isSecondary = false, size = 'sm', ...rest }) {
   const bg = isSecondary ? 'bg-gray-700' : 'bg-white';
   const text = isSecondary ? 'text-white' : 'text-black';
   const px = size === 'xs' ? 4 : 6; 
   return (
     <button
+      {...rest}
       className={`${bg} ${text} font-bold text-xs md:text-${size} px-2 md:px-${px} py-1 md:py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-2 mb-1`}
       type="button"
     >
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShare } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faCopy } from '@fortawesome/free-solid-svg-icons';
+import { Fragment, useState } from 'react';
 import { Pace, WindupChildren } from 'windups';
 
 import Button from './Button.js';
 import Navbar from './Navbar.js';
 
 export default function Hero({ author, children, image, loading }) {
+  const [hasCopied, setHasCopied] = useState(false);
+
+  const updateHasCopied = async () => {
+    await navigator.clipboard.writeText(window.location.href);
+    setHasCopied(!hasCopied);
+  };
+
   return (
     <div
       className="w-full header p-4 md:p-10 md:pl-12 pt-4"
@@ -15,20 +23,23 @@ export default function Hero({ author, children, image, loading }) {
         <Navbar />
         <div className="w-full h-full flex flex-col items-center justify-end">
           {!loading && (
-            <WindupChildren>
-              <Pace getPace={(char) => (char === ' ' ? 40 : 20)}>
-                <div className="text-white text-md md:text-3xl mt-4 mb-2">{children}</div>
-                <div className="text-white text-xs md:text-sm mb-2">
-                  <span className="font-bold">Written By:</span> {author}
-                </div>
-                <div className="flex mt-5">
-                  <Button>
-                    Share
-                    <FontAwesomeIcon className="ml-2" icon={faShare} />
-                  </Button>
-                </div>
-              </Pace>
-            </WindupChildren>
+            <Fragment>
+              <WindupChildren>
+                <Pace getPace={(char) => (char === ' ' ? 40 : 20)}>
+                  <div className="text-white text-md md:text-3xl mt-4 mb-2">{children}</div>
+                  <div className="text-white text-xs md:text-sm mb-2">
+                    <span className="font-bold">Written By:</span> {author}
+                  </div>
+
+                </Pace>
+              </WindupChildren>
+              <div className="flex mt-5">
+                <Button onClick={updateHasCopied}>
+                  {hasCopied ? 'Copied': 'Share'}
+                    <FontAwesomeIcon className="ml-2" icon={hasCopied ? faCheck : faCopy} />
+                </Button>
+              </div>
+            </Fragment>
           )}
         </div>
       </div>
