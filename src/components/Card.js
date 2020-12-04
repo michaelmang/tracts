b837ab0f35f9245de7e4036be09a3f97559db5ec
Mike Mangialardi <mike.mangialardi@rackspace.com>
2020-12-04 11:08:22 -0500

    Open card on click

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,8 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBookOpen } from '@fortawesome/free-solid-svg-icons';
 import kebabcase from 'lodash.kebabcase';
 import { useState } from "react";
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { animated, useSpring } from 'react-spring'
 
-import Button from "./Button.js";
 import { fadeIn } from '../animations.js';
 
 export default function Card({ author, children, description, image, tag }) {
@@ -20,9 +17,16 @@ export default function Card({ author, children, description, image, tag }) {
   const step2 = useSpring(fadeIn({ delay: 250 }));
   const step3 = useSpring(fadeIn({ delay: 350 }));
 
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push(`/tracts/${kebabcase(children)}`)
+  };
+
   return (
     <animated.div
-      className="card w-11/12 md:w-1/5 mb-8 md:mb-1 mr-3 md:mr-1 rounded-lg bg-overlay flex flex-col justify-end items-start text-white p-4"
+      className="card cursor-pointer w-11/12 md:w-1/5 mb-8 md:mb-1 mr-3 md:mr-1 rounded-lg bg-overlay flex flex-col justify-end items-start text-white p-4"
+      onClick={handleClick}
       onMouseEnter={updateHovered(true)}
       onMouseLeave={updateHovered(false)}
       style={{
@@ -40,16 +44,6 @@ export default function Card({ author, children, description, image, tag }) {
         <animated.div style={step2} className="text-sm md:text-2xl font-bold mb-2">{children}</animated.div>
         <animated.div style={step3} className="flex flex-col md:flex-row text-white text-xs"><span className="font-bold text-gray-400 mr-1">Written By:</span> {author}</animated.div>
         {isHovered && <div className="card_description text-xs nmd:text-sm my-6">{description}</div>}
-        {isHovered && (
-          <div className="flex">
-            <Link to={`/tracts/${kebabcase(children)}`}>
-              <Button size="xs">
-                <FontAwesomeIcon className="mr-2" icon={faBookOpen} />
-                Read
-              </Button>
-            </Link>
-          </div>
-        )}
       </div>
     </animated.div>
   );
