7033b96ff11b272ed15dc81389dab3ce231d568c
Mike Mangialardi <mike.mangialardi@rackspace.com>
2020-12-10 09:45:01 -0500

    Update hero image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -7,7 +7,7 @@ import { Pace, WindupChildren } from "windups";
 import Button from './Button.js';
 import Navbar from './Navbar.js';
 
-const defaultImage = "https://images.unsplash.com/photo-1528222354212-a29573cdb844?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80"; 
+const defaultImage = "https://images.unsplash.com/photo-1505072276982-d6c55de5d5d8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"; 
 
 export default function Hero({ author, children, description, image = defaultImage, loading, tag }) {
   return (
